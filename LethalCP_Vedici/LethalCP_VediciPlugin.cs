adc2fe08fee20e8e1588e6ef66bee861f3405154
Kivlan Aziz Al-Falaah <kivlanaziz@gmail.com>
2023-11-27 23:28:38 +0700

    Add Custom Time Scale & Welcome Message

--- LethalCP_Vedici/LethalCP_VediciPlugin.cs
+++ LethalCP_Vedici/LethalCP_VediciPlugin.cs
@@ -7,6 +7,7 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace LethalCP_Vedici
 {
@@ -14,6 +15,7 @@ namespace LethalCP_Vedici
     [BepInPlugin(MyGUID, PluginName, VersionString)]
     public class LethalCP_VediciPlugin : BaseUnityPlugin
     {
+        #region mod desc constant
         // Mod specific details. MyGUID should be unique, and follow the reverse domain pattern
         // e.g.
         // com.mynameororg.pluginname
@@ -23,12 +25,17 @@ namespace LethalCP_Vedici
         private const string MyGUID = "com.kivlan.LethalCP_Vedici";
         private const string PluginName = "LethalCP_Vedici";
         private const string VersionString = "1.0.0";
-
+        #endregion
+        #region config manager
         // Config entry key strings
         // These will appear in the config file created by BepInEx and can also be used
         // by the OnSettingsChange event to determine which setting has changed.
         private static string cfgNightVisionKey = "Night Vision Configuration";
         private static string HideCommandMessagesKey = "Toggle Hide Command Messages";
+        private static string CustomTimeScaleKey = "Toggle Custom Time Scale";
+        private static string UseRandomTimeScaleKey = "Toggle Random Time Scale";
+        private static string MaximumTimeScaleKey = "Maximum Time Scale (Will use this value when random time scale is disabled)";
+        private static string MinimumTimeScaleKey = "Minimum Time Scale";
 
         // Configuration entries. Static, so can be accessed directly elsewhere in code via
         // e.g.
@@ -36,10 +43,14 @@ namespace LethalCP_Vedici
         // TODO Change this code or remove the code if not required.
         private static ConfigEntry<bool> cfgNightVision;
         private static ConfigEntry<bool> HideCommandMessages;
+        private static ConfigEntry<bool> CustomTimeScale;
+        private static ConfigEntry<bool> UseRandomTimeScale;
+        private static ConfigEntry<float> MaximumTimeScale;
+        private static ConfigEntry<float> MinimumTimeScale;
+        #endregion
 
         private static readonly Harmony Harmony = new Harmony(MyGUID);
         public static ManualLogSource Log = new ManualLogSource(PluginName);
-        public static ManualLogSource mls;
 
         private static PlayerControllerB playerRef;
         private static bool nightVision;
@@ -52,13 +63,18 @@ namespace LethalCP_Vedici
         /// </summary>
         private void Awake()
         {
-            cfgNightVision = Config.Bind("Settings", cfgNightVisionKey, false);
-            HideCommandMessages = Config.Bind("Settings", HideCommandMessagesKey, false);
+            cfgNightVision = Config.Bind("Player Settings", cfgNightVisionKey, false);
+            HideCommandMessages = Config.Bind("UI Settings", HideCommandMessagesKey, false);
+            CustomTimeScale = Config.Bind("Time Settings", CustomTimeScaleKey, false);
+            UseRandomTimeScale = Config.Bind("Time Settings", UseRandomTimeScaleKey, false);
+            MaximumTimeScale = Config.Bind("Time Settings", MaximumTimeScaleKey, 2f);
+            MinimumTimeScale = Config.Bind("Time Settings", MinimumTimeScaleKey, 0.5f);
 
             // Apply all of our patches
             Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
             Harmony.PatchAll(typeof(LethalCP_VediciPlugin));
             Harmony.PatchAll(typeof(PlayerControllerB));
+            Harmony.PatchAll(typeof(HUDManager));
             Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
 
             cfgNightVision.SettingChanged += nightVisionCFGChanged;
@@ -94,6 +110,11 @@ namespace LethalCP_Vedici
             }
         }
 
+        /// <summary>
+        /// Method to handle changes to night vision configuration made from config manager
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void nightVisionCFGChanged(object sender, EventArgs e)
         {
             if (!isHost)
@@ -101,8 +122,21 @@ namespace LethalCP_Vedici
                 return;
             }
             nightVision = cfgNightVision.Value;
+
+            if (nightVision)
+            {
+                LethalCP_VediciPlugin.Log.LogInfo("Night Vision Turned On");
+            }
+            else
+            {
+                LethalCP_VediciPlugin.Log.LogInfo("Night Vision Turned Off");
+            }
         }
 
+        /// <summary>
+        /// Method to get the initial configuration of night vision
+        /// </summary>
+        /// <param name="__instance"></param>
         [HarmonyPatch(typeof(PlayerControllerB), "Start")]
         [HarmonyPrefix]
         static void getNightVision(ref PlayerControllerB __instance)
@@ -119,12 +153,15 @@ namespace LethalCP_Vedici
             playerRef.nightVision.range = 10000f;
         }
 
+        /// <summary>
+        /// Method to update the night vision config based on the night vision status. (Updated per frame)
+        /// </summary>
         [HarmonyPatch(typeof(PlayerControllerB), "SetNightVisionEnabled")]
         [HarmonyPostfix]
         static void updateNightVision()
         {
             //instead of enabling/disabling nightvision, set the variables
-
+            
             if (nightVision)
             {
                 playerRef.nightVision.color = UnityEngine.Color.green;
@@ -137,84 +174,115 @@ namespace LethalCP_Vedici
                 playerRef.nightVision.intensity = nightVisionIntensity;
                 playerRef.nightVision.range = nightVisionRange;
             }
-
             // should always be on
             playerRef.nightVision.enabled = true;
         }
 
+        /// <summary>
+        /// Method to update time scale using custom modifier
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPatch(typeof(TimeOfDay), "MoveGlobalTime")]
+        [HarmonyPostfix]
+        static void customizableTimeScale(TimeOfDay __instance)
+        {
+            if (isHost)
+            {
+                if (CustomTimeScale.Value)
+                {
+                    __instance.globalTimeSpeedMultiplier = MaximumTimeScale.Value;
+
+                    if (UseRandomTimeScale.Value)
+                    {
+                        __instance.globalTimeSpeedMultiplier = Random.Range(MinimumTimeScale.Value, MaximumTimeScale.Value);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Display text box on the start of the game
+        /// </summary>
+        [HarmonyPatch(typeof(StartOfRound), "openingDoorsSequence")]
+        [HarmonyPostfix]
+        static void customOpeningScreen()
+        {
+            if (isHost)
+            {
+                string noticeTitle = "Modded Game";
+                string noticeBody = "Made with <3 by Stoichev";
+                HUDManager.Instance.DisplayTip(noticeTitle, noticeBody);
+            }
+        }
         //[HarmonyPatch(typeof(HUDManager), "SubmitChat_performed")]
         //[HarmonyPrefix]
         //static void chatCommand(HUDManager __instance)
         //{
-        //    try
+        //    string text = __instance.chatTextField.text;
+        //    // make prefix even if one doesn't exist
+        //    string tempPrefix = "/";
+        //    mls.LogInfo(text);
+
+        //    // check if prefix is utilized
+        //    if (text.ToLower().StartsWith(tempPrefix.ToLower()))
         //    {
-        //        if (__instance == null)
+        //        string noticeTitle = "Default Title";
+        //        string noticeBody = "Default Body";
+
+        //        if (!isHost)
         //        {
-        //            return;
-        //        }
-
-        //        string text = __instance.chatTextField.text;
-        //        if (text == null)
-        //        {
-        //            return;
-        //        }
-        //        // make prefix even if one doesn't exist
-        //        string tempPrefix = "/";
-        //        mls.LogInfo(text);
-
-        //        // check if prefix is utilized
-        //        if (text.ToLower().StartsWith(tempPrefix.ToLower()))
-        //        {
-        //            string noticeTitle = "Default Title";
-        //            string noticeBody = "Default Body";
-
-        //            if (!isHost)
-        //            {
-        //                noticeTitle = "Command";
-        //                noticeBody = "Unable to send command since you are not host.";
-        //                HUDManager.Instance.DisplayTip(noticeTitle, noticeBody);
-        //                if (HideCommandMessages.Value)
-        //                {
-        //                    __instance.chatTextField.text = "";
-        //                }
-        //                return;
-        //            }
-        //            if (text.ToLower().Contains("night") || text.ToLower().Contains("vision"))
-        //            {
-        //                if (toggleNightVision())
-        //                {
-        //                    noticeBody = "Enabled Night Vision";
-        //                }
-        //                else
-        //                {
-        //                    noticeBody = "Disabled Night Vision";
-        //                }
-        //                noticeTitle = "Night Vision";
-        //            }
-        //            // sends notice to user about what they have done
-        //            //HUDManager.Instance.DisplayTip(noticeTitle, noticeBody);
-
-        //            // ensures value is hidden if set but path doesn't hide it
+        //            noticeTitle = "Command";
+        //            noticeBody = "Unable to send command since you are not host.";
+        //            HUDManager.Instance.DisplayTip(noticeTitle, noticeBody);
         //            if (HideCommandMessages.Value)
         //            {
         //                __instance.chatTextField.text = "";
         //            }
         //            return;
         //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        mls.LogError(ex);
+        //        if (text.ToLower().Contains("night") || text.ToLower().Contains("vision"))
+        //        {
+        //            if (toggleNightVision())
+        //            {
+        //                noticeBody = "Enabled Night Vision";
+        //            }
+        //            else
+        //            {
+        //                noticeBody = "Disabled Night Vision";
+        //            }
+        //            noticeTitle = "Night Vision";
+        //        }
+        //        // sends notice to user about what they have done
+        //        HUDManager.Instance.DisplayTip(noticeTitle, noticeBody);
+
+        //        // ensures value is hidden if set but path doesn't hide it
+        //        if (HideCommandMessages.Value)
+        //        {
+        //            __instance.chatTextField.text = "";
+        //        }
         //        return;
         //    }
         //}
 
+        /// <summary>
+        /// Method to toggle night vision status
+        /// </summary>
         private static bool toggleNightVision()
         {
+
             if (isHost)
             {
                 nightVision = !nightVision;
                 cfgNightVision.Value = nightVision;
+            }
+
+            if (nightVision)
+            {
+                LethalCP_VediciPlugin.Log.LogInfo("Night Vision Turned On");
+            }
+            else
+            {
+                LethalCP_VediciPlugin.Log.LogInfo("Night Vision Turned Off");
             }
 
             return nightVision;
